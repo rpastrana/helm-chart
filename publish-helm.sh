80d4f618a51cf79e131555fd476c4f7ce9b1387d
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexisrisk.com>
2021-04-30 12:44:54 -0400

    HPCC Helm Charts Release Candidate 0
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexisrisk.com>

--- publish-helm.sh
+++ publish-helm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-HPCC_DIR=$1
+HPCC_DIR=/c/assignments/GIT/HPCC-Platform
 [[ -z ${HPCC_DIR} ]] && HPCC_DIR=~/HPCC-Platform
 
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $DIR/docs 2>&1 > /dev/null
+echo "$DIR"
+#pushd $DIR/docs 2>&1 > /dev/null
 
+echo "helm package ${HPCC_DIR}/dockerfiles/hpcc/"
 helm package ${HPCC_DIR}/dockerfiles/hpcc/
-helm repo index . --url https://hpcc-systems.github.io/helm-chart
+echo "helm repo index . --url https://rpastrana.github.io/helm-chart"
+helm repo index . --url https://rpastrana.github.io/helm-chart
